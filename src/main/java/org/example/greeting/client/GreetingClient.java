3a9b0c99810f6c031bf6fa097c3128acdb500b05
esehmec <esehmec@gmail.com>
2024-06-04 09:17:16 -0500

    Readme

--- src/main/java/org/example/greeting/client/GreetingClient.java
+++ src/main/java/org/example/greeting/client/GreetingClient.java
@@ -10,16 +10,19 @@ public class GreetingClient {
 
     public static void main(String[] args) {
 
+        // Check if the "greet" command is sent as an argument. Otherwise, fail.
         if (args.length == 0) {
             System.out.println("Need argument to work");
             return;
         }
 
+        // Build a channel that listens to the server's port
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
+        // Call doGreet method if the first argument is "greet"
         switch (args[0]) {
             case "greet": doGreet(channel);
             break;
@@ -33,6 +36,7 @@ public class GreetingClient {
 
     }
 
+    // Build a GRPC stub and call greet method from the service side.
     private static void doGreet(ManagedChannel channel){
         System.out.println("Entered doGreet!");
         GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
